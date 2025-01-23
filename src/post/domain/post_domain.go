619cda615dd85e1b5bbed01d75ebac4fe980fe4d
Buyung Hardiansyah <buyung.hardiansyah@gmail.com>
2025-01-23 18:45:16 +0700

    add category id in create post

--- src/post/domain/post_domain.go
+++ src/post/domain/post_domain.go
@@ -4,5 +4,6 @@ type Post struct {
 	ID          int    `json:"id" gorm:"id,omitempty"`
 	Title       string `json:"title" gorm:"title"`
 	Description string `json:"description" gorm:"description"`
+	CategoryID  int    `json:"category_id" gorm:"category_id"`
 	CreatedDate string `json:"created_date" gorm:"created_date"`
 }
--- src/post/repository/post_repository.go
+++ src/post/repository/post_repository.go
@@ -1,14 +1,17 @@
 package post
 
 import (
+	"fmt"
 	"postergist-api/helper"
+	categoryRepository "postergist-api/src/category/repository"
 	domain "postergist-api/src/post/domain"
 
 	"gorm.io/gorm"
 )
 
 type postRepository struct {
-	DB *gorm.DB
+	DB      *gorm.DB
+	catRepo categoryRepository.CategoryRepository
 }
 
 type PostRepository interface {
@@ -18,8 +21,10 @@ type PostRepository interface {
 }
 
 func NewPostRepository(db *gorm.DB) PostRepository {
+	catRepository := categoryRepository.NewCategoryRepository(db)
 	return &postRepository{
-		DB: db,
+		DB:      db,
+		catRepo: catRepository,
 	}
 }
 
@@ -36,6 +41,13 @@ func (p *postRepository) GetPosts() ([]domain.Post, error) {
 
 func (p *postRepository) CreatePosts(dp domain.Post) error {
 	dp.CreatedDate = helper.GetCurrentDateTime()
+	isCatExists, err := p.catRepo.GetCategory(dp.CategoryID)
+	if err != nil {
+		return fmt.Errorf("category not found")
+	}
+	if isCatExists.ID == 0 {
+		return fmt.Errorf("category id is null")
+	}
 	tx := p.DB.Create(&dp)
 	return tx.Error
 }
